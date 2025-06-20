cdaf8fdcef8201bf5008ab5ee47cf1d5a3be3716
chrisshuo123 <chr1z100@gmail.com>
2025-06-20 20:53:24 +0700

    rename json from json:'idMerek_fk' to merek, and json:'idJenis_fk' to jenis on Cars struct, to help with CreateCarsController issues for executing 'null' brand (merek) and types (jenis).  On the relationships, the json setted to json:'-'.

--- backend/models/cars.go
+++ backend/models/cars.go
@@ -3,17 +3,17 @@ package models
 type Cars struct {
 	CarsID     uint   `gorm:"primaryKey;column:idCars" json:"idCars" form:"idCars"`
 	NamaMobil  string `gorm:"not null;column:nama_mobil" json:"nama_mobil" form:"nama_mobil"`
-	MerekID    *uint  `gorm:"column:idMerek_fk" json:"-" form:"-"`
-	JenisID    *uint  `gorm:"column:idJenis_fk" json:"-" form:"-"`
+	MerekID    *uint  `gorm:"column:idMerek_fk" json:"merek" form:"merek"`
+	JenisID    *uint  `gorm:"column:idJenis_fk" json:"jenis" form:"jenis"`
 	HorsePower uint   `gorm:"column:horse_power" json:"horse_power" form:"horse_power"`
 
 	// Relationships
-	Merek *Merek `gorm:"foreignKey:idMerek_fk;references:idMerek" json:"merek"`
-	Jenis *Jenis `gorm:"foreignKey:idJenis_fk;references:idJenis" json:"jenis"`
+	Merek *Merek `gorm:"foreignKey:idMerek_fk;references:idMerek" json:"-"`
+	Jenis *Jenis `gorm:"foreignKey:idJenis_fk;references:idJenis" json:"-"`
 }
 
 type Merek struct {
-	ID   uint   `gorm:"column:idMerek;primaryKey" json:"id" form:"id"`
+	ID   uint   `gorm:"column:idMerek;primaryKey" json:"idMerek" form:"id"`
 	Nama string `gorm:"column:merek" json:"merek" form:"merek"`
 }
 
@@ -23,6 +23,6 @@ func (Merek) TableName() string {
 }
 
 type Jenis struct {
-	ID   uint   `gorm:"column:idJenis;primaryKey" json:"id" form:"id"`
+	ID   uint   `gorm:"column:idJenis;primaryKey" json:"idJenis" form:"id"`
 	Nama string `gorm:"column:jenis" json:"jenis" form:"jenis"`
 }
