dc9de4c7f9e856b6c239433f58336049b7a614e7
chrisshuo123 <chr1z100@gmail.com>
2025-06-20 21:06:17 +0700

    Refining the CORS, and add routes for api brands (merek) and types (jenis)

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -10,12 +10,17 @@ import (
 
 func InitRoute(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://127.0.0.1:5500", "http://localhost:5500"},
-		AllowMethods: []string{http.MethodGet, http.MethodPost},
+		//AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:5500"},
+		AllowOrigins:     []string{"*"}, // Allow all origins during development
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowCredentials: true,
 	}))
 
 	e.Static("/", "frontend/views")
 
 	e.POST("/api/cars", controllers.CreateCarsController)
 	e.GET("/api/cars", controllers.GetCarsController)
+	e.GET("/api/brands", controllers.GetMerekController)
+	e.GET("/api/types", controllers.GetJenisController)
 }
