b832efa3bbadef2ad67198efbb2a87ea03fcc941
chrisshuo123 <chr1z100@gmail.com>
2025-06-21 16:56:24 +0700

    Add http.MethodPut to allow making updates in the AllowMethods CORS section, while add 2 new routes for GetCarController (without 'Cars'), and UpdateCarController to function well in the RESTAPI (such as PostMan) and Website.

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -12,7 +12,7 @@ func InitRoute(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		//AllowOrigins:     []string{"http://127.0.0.1:5500", "http://localhost:5500"},
 		AllowOrigins:     []string{"*"}, // Allow all origins during development
-		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodOptions, http.MethodPut},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowCredentials: true,
 	}))
@@ -20,7 +20,9 @@ func InitRoute(e *echo.Echo) {
 	e.Static("/", "frontend/views")
 
 	e.POST("/api/cars", controllers.CreateCarsController)
-	e.GET("/api/cars", controllers.GetCarsController)
+	e.GET("/api/cars", controllers.GetCarsController)    // List all cars
+	e.GET("/api/cars/:id", controllers.GetCarController) // For single car, outmostly supporting the update func
 	e.GET("/api/brands", controllers.GetMerekController)
 	e.GET("/api/types", controllers.GetJenisController)
+	e.PUT("/api/cars/:id", controllers.UpdateCarController)
 }
